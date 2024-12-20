7cb0fd16ccde2649c8a266df6022c0ed42387fc1
shanjian <shanjian@codemao.com>
2024-12-20 14:18:40 +0800

    feat:add spring aop to do work

--- spring-aop/src/main/java/org/springframework/aop/config/AspectJAutoProxyBeanDefinitionParser.java
+++ spring-aop/src/main/java/org/springframework/aop/config/AspectJAutoProxyBeanDefinitionParser.java
@@ -41,7 +41,9 @@ class AspectJAutoProxyBeanDefinitionParser implements BeanDefinitionParser {
 	@Override
 	@Nullable
 	public BeanDefinition parse(Element element, ParserContext parserContext) {
+		// 注册  <aop:aspectj-autoproxy/>
 		AopNamespaceUtils.registerAspectJAnnotationAutoProxyCreatorIfNecessary(parserContext, element);
+		// 子类解析
 		extendBeanDefinition(element, parserContext);
 		return null;
 	}
